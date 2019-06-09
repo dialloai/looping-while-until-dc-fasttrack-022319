c0796a7603e942457100df77817cf83834e6f78c
Aicha D <aicha1diallo@gmail.com>
2019-06-09 17:12:11 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,4 +1,4 @@
-def using_until
+#def using_until
   levitation_force = 6
   #your code here
     
@@ -6,7 +6,7 @@ end
 
 levitation_force = 6
 	
-	 until levitation_force <= 10
+	 using_until puts levitation_force = 10
     puts "Wingardium Leviosa"
     levitation_force = levitation_force + 1
   end
