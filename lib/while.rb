90ff79bf3b3a17fb0d0be7751672e8d393f31db4
Aicha D <aicha1diallo@gmail.com>
2019-06-09 17:21:08 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,7 +2,7 @@ def using_while
   # this counts when to break the loop
   levitation_force = 6
 	
-  until levitation_force <= 10
+  while levitation_force <= 10
     puts "Wingardium Leviosa"
     levitation_force = levitation_force + 1
   end
